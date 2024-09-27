6e6311dbc9f122d3f8f1078189fac37ad157d3ba
LakshithaD10 <163376193+LakshithaD10@users.noreply.github.com>
2024-09-27 12:38:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ $(".Contact_Me_Button.Styled").click(function () {
 
 $(".Resume-Button").click(function () {
   // Replace 'path/to/your.pdf' with the actual path to your PDF file
-  var pdfUrl = "./public/CV - Lakshitha Dassanayake.pdf";
+  var pdfUrl = "./public/Resume - Lakshitha Dassanayake.pdf";
 
   // Create a hidden link to the PDF
   var link = document.createElement("a");
   link.href = pdfUrl;
-  link.download = "CV - Lakshitha Dassanayake.pdf"; // Set the desired file name
+  link.download = "Resume - Lakshitha Dassanayake.pdf"; // Set the desired file name
 
   // Simulate a click on the link to trigger the download
   document.body.appendChild(link);
